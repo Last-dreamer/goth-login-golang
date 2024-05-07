7a1a9add2b8a34480175caee6293ff0dac90f071
Lost Dreamer <asim0000.ak@gmail.com>
2024-05-07 12:35:15 +0400

    Update main.go
    fixing the sessions

--- main.go
+++ main.go
@@ -14,11 +14,12 @@ import (
 	"github.com/markbates/goth/gothic"
 	"github.com/markbates/goth/providers/google"
 )
+ 
+var store = sessions.NewCookieStore([]byte(strconv.Itoa(8888)))
 
 func main() {
+	// key := RandomInteger(7)
 
-	key := RandomInteger(7)
-	store := sessions.NewCookieStore([]byte(strconv.Itoa(key)))
 	store.MaxAge(86400 * 30) // 30 days
 	store.Options.Path = "/"
 	store.Options.HttpOnly = true
@@ -40,6 +41,7 @@ func main() {
 	r.GET("/auth/google", Auth)
 
 	r.GET("/auth/google/callback", AuthCallback)
+	r.GET("/getUserDetail", GetUser)
 
 	r.Run(":8080")
 }
@@ -64,11 +66,34 @@ func Auth(cntx *gin.Context) {
 	gothic.BeginAuthHandler(cntx.Writer, cntx.Request)
 }
 
+func GetUser(c *gin.Context) {
+
+	session, _ := store.Get(c.Request, "user_session") // Ensure you have initialized 'store'
+	userValue, ok := session.Values["user"]
+	if !ok || userValue == nil {
+		// Handle the case where there is no user in the session
+		// For example, redirect to the login page or return an error response
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "User not logged in"})
+		return
+	}
+	user, ok := userValue.(goth.User)
+	if !ok {
+		// Handle the case where the session value is not of type goth.User
+		// This should not happen if your session management is correct
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		return
+	}
+	// Now you can safely use the 'user' object
+	// For example, return the user's information as a JSON response
+	c.JSON(http.StatusOK, gin.H{"user": user})
+}
+
+// testing for flutter
 func AuthCallback(cntx *gin.Context) {
 
 	// Pull the provider from the path parameters
 	query := cntx.Request.URL.Query()
-	query.Add("provider", cntx.Param("provider"))
+	query.Add("provider", "google")
 	cntx.Request.URL.RawQuery = query.Encode()
 
 	gothic.SetState(cntx.Request)
@@ -76,12 +101,16 @@ func AuthCallback(cntx *gin.Context) {
 	// Call CompleteUserAuth to have it process the response
 	gothUser, err := gothic.CompleteUserAuth(cntx.Writer, cntx.Request)
 	if err != nil {
-		log.Println(err)
+		log.Println("error while getting goth User", err)
 		return
 	}
 
-	log.Println("testing goth User ", gothUser.ExpiresAt)
-	// Save the needed information from the response
-	// cntx.HTML(http.StatusOK, "success", nil)
-	cntx.Redirect(http.StatusPermanentRedirect, "http://20.203.31.58/dashboard/default")
+	log.Println("testing gothUser ", gothUser)
+	session, _ := store.Get(cntx.Request, "user_session")
+	session.Values["user"] = gothUser
+	session.Save(cntx.Request, cntx.Writer)
+
+	cntx.JSON(200, gothUser)
+	// cntx.Redirect(http.StatusPermanentRedirect, "http://20.203.31.58/dashboard/default")
+
 }
